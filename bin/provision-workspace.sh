aacf66a195b1c3a30394fb4d91008043ef2bd5f9
Ryan Wholey <ryan@takescoop.com>
2021-06-23 12:50:47 -0600

    message

--- bin/provision-workspace.sh
+++ bin/provision-workspace.sh
@@ -6,13 +6,13 @@ mkdir -p terraform-workspace
 
 cd terraform-workspace
 
+cp $DEFAULT_VARS .
+cp $PROJECT_VARS .
 cp $TEMPLATE_DIR/backend.tf .
 
 terraform init
 terraform plan
 
-# cp $DEFAULT_VARS .
-# cp $PROJECT_VARS .
 
 # ls 
 
