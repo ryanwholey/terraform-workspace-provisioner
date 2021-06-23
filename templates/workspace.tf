b93bb0dd708c0dce6356ed273ec0dda6524a85ca
Ryan Wholey <ryan@takescoop.com>
2021-06-23 12:36:57 -0600

    message

--- templates/workspace.tf
+++ templates/workspace.tf
@@ -1,7 +1,6 @@
 variable "terraform_organization" {}
 
 variable "secrets_workspace" {}
-variable "secrets_key" {}
 
 variable "backend_storage_name" {}
 variable "workspace_name" {}
@@ -19,15 +18,17 @@ data "terraform_remote_state" "secrets" {
 module "project" {
   source  = "github.com/ryanwholey/terraform-workspace-manager//modules/project"
 
-  secrets      = data.terraform_remote_state.secrets.outputs[var.secrets_key]
+  secrets      = data.terraform_remote_state.secrets.outputs.secrets
   name         = var.workspace_name
   organization = var.terraform_organization
 }
 
 terraform {
   backend "s3" {
-    bucket = data.terraform_remote_state.secrets.outputs.storage_bucket
-    key    = "${var.workspace_name}.tfstate"
-    region = "us-west-2"
+    bucket         = data.terraform_remote_state.secrets.outputs.storage.bucket
+    aws_access_key = data.terraform_remote_state.secrets.outputs.storage.access_key
+    aws_secret_key = data.terraform_remote_state.secrets.outputs.storage.secret_key
+    key            = "${var.workspace_name}.tfstate"
+    region         = "us-west-2"
   }
 }
