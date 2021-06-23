a172f8309ee97c7d0996d54089640cf709756073
Ryan Wholey <ryan@takescoop.com>
2021-06-23 12:59:38 -0600

    message

--- templates/backend.tf
+++ templates/backend.tf
@@ -12,9 +12,9 @@ resource "local_file" "backend" {
     content = <<-EOF
       terraform {
         backend "s3" {
-          bucket     = ${data.terraform_remote_state.secrets.outputs.storage.bucket}
-          access_key = ${data.terraform_remote_state.secrets.outputs.storage.access_key}
-          secret_key = ${data.terraform_remote_state.secrets.outputs.storage.secret_key}
+          bucket     = "${data.terraform_remote_state.secrets.outputs.storage.bucket}"
+          access_key = "${data.terraform_remote_state.secrets.outputs.storage.access_key}"
+          secret_key = "${data.terraform_remote_state.secrets.outputs.storage.secret_key}"
           key        = "${var.workspace_name}.tfstate"
           region     = "us-west-2"
         }
