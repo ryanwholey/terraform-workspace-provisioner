693e75c661ddb10a5dd1bcc8321ecdf9e7294e1c
Ryan Wholey <ryan@takescoop.com>
2021-06-23 10:46:53 -0600

    message

--- bin/setup-terraform.sh
+++ bin/setup-terraform.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 curl -o terraform.zip https://releases.hashicorp.com/terraform/0.15.5/terraform_0.15.5_linux_amd64.zip 
-unzip -f -d /usr/local/bin terraform.zip
-terraform version
+unzip terraform.zip
+chmod +x terraform
+mv terraform /usr/local/bin/tf
+tf version
