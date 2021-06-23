f799283296b5a533636c2d7a711bd2214ec1abec
Ryan Wholey <ryan@takescoop.com>
2021-06-23 10:41:25 -0600

    unzip terraform

--- bin/setup-terraform.sh
+++ bin/setup-terraform.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-which curl || true
-which wget || true
-which sudo || true
-
-# apt-get update && apt-get install -y gnupg software-properties-common curl
-# curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
-# sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-# sudo apt-get update && sudo apt-get install terraform
+curl -o terraform.zip https://releases.hashicorp.com/terraform/0.15.5/terraform_0.15.5_linux_amd64.zip 
+unzip terraform.zip
+ls
