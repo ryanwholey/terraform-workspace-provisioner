8741a6f7c654dda3ec71357a45efbee61d0e4143
Ryan Wholey <ryan@takescoop.com>
2021-06-23 13:11:45 -0600

    message

--- bin/provision-workspace.sh
+++ bin/provision-workspace.sh
@@ -4,9 +4,8 @@ set -x
 
 terraform version
 
-mkdir -p terraform-workspace
-
-cd terraform-workspace
+mkdir -p create-backend
+cd create-backend
 
 cp $DEFAULT_VARS .
 cp $PROJECT_VARS .
@@ -16,11 +15,18 @@ cp $TEMPLATE_DIR/backend.tf .
 terraform init
 terraform apply -auto-approve
 
-ls -a
+cd ../
 
+mkdir -p terraform-workspace
+cd terraform-workspace
 
+cp $DEFAULT_VARS .
+cp $PROJECT_VARS .
+cp $TEMPLATE_DIR/basic-vars.tf .
 cp $TEMPLATE_DIR/workspace.tf .
+cp ../create-backend/backend.tf .
 
+ls -a
 
 terraform init
 terraform plan
