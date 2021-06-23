6264a3f663369f7b27d231917c570de41fd9fc67
Ryan Wholey <ryan@takescoop.com>
2021-06-23 10:39:17 -0600

    see whats going on

--- bin/setup-terraform.sh
+++ bin/setup-terraform.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-echo bin script
+which curl || true
+which wget || true
+which sudo || true
+
+# apt-get update && apt-get install -y gnupg software-properties-common curl
+# curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
+# sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+# sudo apt-get update && sudo apt-get install terraform
